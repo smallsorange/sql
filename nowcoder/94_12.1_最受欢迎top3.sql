0efea784c01dc62a26ccf820e94fccd3db8f8b18
smallsorange <107605082+smallsorange@users.noreply.github.com>
2025-12-01 14:19:18 +0800

    Create 94_12.1_最受欢迎top3.sql

---
+++ nowcoder/94_12.1_最受欢迎top3.sql
@@ -0,0 +1,16 @@
+select cid,pv,time_len
+from (
+select c.cid,count(uid) as pv,avg(score) as average_score,sum(timestampdiff(minute,start_time,end_time)) time_len
+from play_record_tb p join course_info_tb c on p.cid=c.cid
+where datediff(date(start_time),release_date)<=7
+group by cid
+) t
+where average_score >=3
+order by pv desc
+limit 3
+-- # 1.from p join c 
+-- # 2.where datediff(date(start_time),release_date)<=7. date_sub()
+-- # 3.group by cid
+-- # 4.count,avg,sum(timestampdiff)
+-- # 5.from 子查询
+-- # 6.where 
